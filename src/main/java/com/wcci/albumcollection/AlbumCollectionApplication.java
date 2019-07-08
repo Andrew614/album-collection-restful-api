a843166440fbc2afaa316ff70841e6b7d2161acb
matthew-laine <matthew.i.laine@gmail.com>
2019-07-08 13:19:00 -0400

    initial commit

---
+++ src/main/java/com/wcci/albumcollection/AlbumCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.wcci.albumcollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumCollectionApplication.class, args);
+	}
+
+}
