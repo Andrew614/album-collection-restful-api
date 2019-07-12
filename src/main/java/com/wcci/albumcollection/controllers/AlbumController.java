4eb1afe2cbcc04cfd0324f15d8bf370d96a91ccd
Sshipley12 <steph.shipley@yahoo.com>
2019-07-12 14:30:35 -0400

    added album controller get mappings and test

--- src/main/java/com/wcci/albumcollection/controllers/AlbumController.java
+++ src/main/java/com/wcci/albumcollection/controllers/AlbumController.java
@@ -2,20 +2,24 @@ package com.wcci.albumcollection.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.wcci.albumcollection.entities.Album;
 import com.wcci.albumcollection.repositories.AlbumRepository;
 
-
+@RestController
+@RequestMapping("/api")
 public class AlbumController {
 
 	@Autowired
 	private AlbumRepository albumRepo;
 
+	@RequestMapping("/albums")
 	public Iterable<Album> findAllAlbums() {
 		return albumRepo.findAll();
 	}
-
+	@RequestMapping("/albums/{id}")
 	public Album findOneAlbum(@PathVariable Long id) {
 		return albumRepo.findById(id).get();
 	}
--- src/test/java/com/wcci/albumcollection/controllertests/web_layer_test/AlbumControllerWebLayerTest.java
+++ src/test/java/com/wcci/albumcollection/controllertests/web_layer_test/AlbumControllerWebLayerTest.java
@@ -1,8 +1,17 @@
 package com.wcci.albumcollection.controllertests.web_layer_test;
 
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Optional;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -33,10 +42,32 @@ public class AlbumControllerWebLayerTest {
 	
 	@Before
 	public void setup() {
+		song1 = new Song("title1", "link1", "time1");
+		song2 = new Song("title2", "link2", "time2");
+		songCollection = new ArrayList<Song>();
 		songCollection.add(song1);
 		songCollection.add(song2);
 		testAlbum = new Album("title", "imageUrl", songCollection, "recordLabel");
 		
+	}
+	@Test
+	public void fetchCollectionOfAlbums() throws Exception {
+		when(albumRepo.findAll()).thenReturn(Collections.singletonList(testAlbum));
+		mockMvc.perform(get("/api/albums"))
+		.andExpect(status().isOk())
+		.andExpect(content().contentType("application/json;charset=UTF-8"))
+		.andExpect(content().json("[{}]"))
+		.andExpect(content().json(mapper.writeValueAsString(Collections.singletonList(testAlbum)), true));
+	}
+	
+	@Test
+	public void fetchSingleAlbum() throws Exception {
+		when(albumRepo.findById(1L)).thenReturn(Optional.of(testAlbum));
+		mockMvc.perform(get("/api/albums/1"))
+		.andExpect(status().isOk())
+		.andExpect(content().contentType("application/json;charset=UTF-8"))
+		.andExpect(content().json("{}"))
+		.andExpect(content().json(mapper.writeValueAsString(testAlbum), true));
 	}
 	
 	
