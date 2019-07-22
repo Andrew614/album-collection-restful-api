f1787db819c5243965cc3488ab428d8373ce895c
Andrew614 <azhang213@gmail.com>
2019-07-22 14:44:20 -0400

    add album form and footer

--- front-end/js/utils/Components/Components.js
+++ front-end/js/utils/Components/Components.js
@@ -1,4 +1,4 @@
-import Html from "../html";
+import Html from '../Html/html';
 import Api from "..Api/Api"
 
 export default () => new Components()
@@ -7,17 +7,47 @@ class Components {
     getAppContext() {
         return Html().select("#app");
     }
+
     getWrapperDiv() {
         return Html()
             .create("div")
             .addClass("wrapper");
     }
+
+    renderAlbumForm() {
+        const formContainer = Html().create('section').addClass('form-container');
+        const form = Html().create('form').addAttribute('action', '#');
+        const formTitle = Html().create('h2').addClass('form__title').text('Input an album');
+        const labelSelect = Html().create('label').addClass('form__label').text('Artist Name:');
+        const selectOption = Html().create('select');
+        form.addChild(formTitle)
+        form.addAttribute(labelSelect)
+
+        //need to come back and add artist options when we have API built out
+
+        form.addAttribute(selectOption)
+
+        const inputList = ['Title', 'ImageUrl', 'Record Label']
+        inputList.forEach((index) => {
+            const label = Html().create('label').addClass('form__label').text(index);
+            const input = Html().create('input').addClass('form__input').addAttribute('name', index).addAttribute('type', 'text').addAttribute('required', '');
+            form.addChild(label.addChild(input))
+        })
+        const button = Html().create('button').addChild('form__button');
+        form.addChild(button).text('Submit')
+        //this will go last
+        formContainer.addChild(form);
+
+        return formContainer;
+    }
+
     renderHeaderBlock(requestData) {
         const headerBlock = Html().create('header').addClass('main-header');
         const titleBlock = Html().create('h1').addClass('main-header__title').text("Album")
         headerBlock.addChild(titleBlock);
         return headerBlock
     }
+
     renderNavBlock() {
         const navBlock = Html().create('nav').addClass('nav');
         const navList = Html().create('ul').addClass('nav_list');
@@ -32,8 +62,7 @@ class Components {
                         event.preventDefault()
                         this.renderPageHome()
 
-                    }
-                    )
+                    })
             )
         const navListItemTwo = Html().create('li')
             .addClass('nav__list-item')
@@ -45,8 +74,7 @@ class Components {
                         event.preventDefault()
                         this.renderPageArtist()
 
-                    }
-                    )
+                    })
             )
         const navListItemThree = Html().create('li')
             .addClass('nav__list-item')
@@ -58,8 +86,7 @@ class Components {
                         event.preventDefault()
                         this.renderPageArtist()
 
-                    }
-                    )
+                    })
             )
         const navListItemFour = Html().create('li')
             .addClass('nav__list-item')
@@ -71,8 +98,7 @@ class Components {
                         event.preventDefault()
                         this.renderPageSong()
 
-                    }
-                    )
+                    })
             )
         navList.addChild(navListItemOne)
         navList.addChild(navListItemTwo)
@@ -82,5 +108,11 @@ class Components {
         return navBlock;
     }
 
+    renderFooter() {
+        const footer = Html().create('footer').addClass('footer')
+        const footerText = Html.create('small').addClass('footer__text').text('&copy; 2019');
+        footer.addChild(footer);
+        return footer;
+    }
 
 }
